eb2cbc393a89465ae1fb3f157c7a114efeca4bb3
Arjun <apmenon2@illinois.edu>
2020-02-01 19:34:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col,  Card,
+  CardHeader,
+  CardTitle,
+  CardImg,
+  CardBody,
+  CardFooter,
+  Button } from "shards-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col md="12" className="header-container">
+        HELLO
+        </Col>
+      </Row>
+      <Row>
+        <Col sm="12" md="2" lg="3">
+        </Col>
+        <Col sm="12" md="8" lg="6">
+          <Card>
+            <CardBody>
+              Nunc quis nisl ac justo elementum sagittis in quis justo.
+            </CardBody>
+          </Card>
+        </Col>
+        <Col sm="12" md="2" lg="3">
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
